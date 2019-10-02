0a5b5f7b3480180bdaf2160090357e18a417893d
Alen <Alenslo@gmx.net>
2019-10-02 10:57:36 +0200

    circles are randomly created

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ function createCircle() {
     var c = document.querySelector("#myCanvas");
     var ctx = c.getContext("2d");
     ctx.beginPath();
-    ctx.arc(100, 200, 50, 0, 2 * Math.PI); //x,y, radius 
+    ctx.arc(randPos().xRand, randPos().yRand, 50, 0, 2 * Math.PI); //x,y, radius 
     //ctx.stroke();
     ctx.fillStyle = createColor();
     ctx.fill();
     
 }
+
+function randPos() {
+    let x = 0;
+    let y = 0;
+    let xRand =  Math.random(x)*1200;
+    let yRand =  Math.random(y)*600;
+    return {xRand, yRand};
+} 
+
 
 function createColor() {
     let resultRed = Math.floor(Math.random()*256);
@@ -22,3 +31,4 @@ function createColor() {
     let resultBlue = Math.floor(Math.random()*256);
     return 'rgb(' +resultRed+ ',' +resultGreen+ ',' +resultBlue+ ')'; 
 }
+
