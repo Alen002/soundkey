c8ec448c5f194392197c2212b2b3c7e6f5c15253
Alen <Alenslo@gmx.net>
2019-10-02 15:36:55 +0200

    random sounds are played when key is pressed

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-
-
-
-document.onkeypress = () => {createCircle()};
-
-
+document.onkeypress = () => {createCircle(), playAudio()};
 
 function createCircle() {
     var c = document.querySelector("#myCanvas");
-    var ctx = c.getContext("2d");
+    ctx = c.getContext("2d");
     ctx.beginPath();
-    ctx.arc(randPos().xRand, randPos().yRand, 50, 0, 2 * Math.PI); //x,y, radius 
-    //ctx.stroke();
-    ctx.fillStyle = createColor();
-    ctx.fill();
+    z = 0;
+    x = randPos().xRand;
+    y = randPos().yRand;
+    color = createColor();
+ 
+    let interval = setInterval(function drawCircle() { 
+        z++;
+        ctx.arc(x, y, z, 0, 2 * Math.PI); //x,y, radius
     
+        ctx.fillStyle = color;
+        ctx.fill();
+       
+        if (z > 250) clearInterval(interval);
+        
+    }, 1); 
+
+    
+}//**************************************** */
+
+function playAudio() {
+    playlist = arraySounds();
+    let select = Math.round(Math.random()*25);
+    console.log(select)
+    let audio = new Audio(playlist[select]);
+    audio.play();
+}
+
+function circleRadius() {
+    for(let a = 0; a < 100; a++) {
+        a += 1;
+       return a;
+    }   
 }
 
 function randPos() {
@@ -24,7 +46,6 @@ function randPos() {
     return {xRand, yRand};
 } 
 
-
 function createColor() {
     let resultRed = Math.floor(Math.random()*256);
     let resultGreen = Math.floor(Math.random()*256);
@@ -32,3 +53,35 @@ function createColor() {
     return 'rgb(' +resultRed+ ',' +resultGreen+ ',' +resultBlue+ ')'; 
 }
 
+
+function arraySounds()  {
+    let sounds = [
+        './sounds/bubbles.mp3',
+        './sounds/clay.mp3',
+        './sounds/confetti.mp3',
+        './sounds/corona.mp3',
+        './sounds/dotted-spiral.mp3',
+        './sounds/flash-1.mp3',
+        './sounds/flash-2.mp3',
+        './sounds/flash-3.mp3',
+        './sounds/glimmer.mp3',
+        './sounds/moon.mp3',
+        './sounds/pinwheel.mp3',
+        './sounds/piston-1.mp3',
+        './sounds/piston-2.mp3',
+        './sounds/piston-3.mp3',
+        './sounds/prism-1.mp3',
+        './sounds/prism-2.mp3',
+        './sounds/prism-3.mp3',
+        './sounds/splits.mp3',
+        './sounds/squiggle.mp3',
+        './sounds/strike.mp3',
+        './sounds/suspension.mp3',
+        './sounds/timer.mp3',
+        './sounds/ufo.mp3',
+        './sounds/veil.mp3',
+        './sounds/wipe.mp3',
+        './sounds/zig-zag.mp3',
+    ];
+    return sounds;
+}
